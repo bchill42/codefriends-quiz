1b0daee2545ea78b6b7736e25d748f12855eab84
Aaron Larner <anlarner@gmail.com>
2020-07-11 12:34:45 -0400

    Proposed changes

--- quiz.js
+++ quiz.js
@@ -7,26 +7,22 @@ let quizTime = 0;
 let numWrong = 0;
 let numCorrect = 0;
 
-for (i = 0; i < 3; i++) {
+for (let i = 0; i < questions.length; i++) {
     let difficulty = 1;
-    tstart = new Date().getTime();
     //console.log(tstart);
-    let userAnswer = prompt(questions[i].question);
-    if (typeof(userAnswer) === "string") {
-        userAnswer = userAnswer.toLowerCase();
-    }
+    let userAnswer = prompt(questions[i].question).toLowerCase();
     if (userAnswer === questions[i].answer) {
         console.log('Correct! The answer is',questions[i].answer);
         numCorrect++;
         difficulty++;
-        } else {
+    } else {
         console.log('Sorry',userAnswer,'is incorrect. The correct answer is',questions[i].answer,'.')
         numWrong++;
     }
     tend = new Date().getTime();
     //console.log(tend);
-    quizTime = quizTime + (tend-tstart);
 }
+quizTime = tend-tstart;
 console.log('You answered',numCorrect,'question correct');
 console.log('You answered',numWrong,'question incorrect');
 console.log('You took',quizTime/1000,'seconds.')
